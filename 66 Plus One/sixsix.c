ee3acde4f33260b38f71f8d6b6806b456fa479a0
Arfaz Hussain <arfazhussain@uvic.ca>
2023-01-22 22:48:11 -0800

    Routine Commit Session Committed on: [Sun 22 Jan 2023 22:48:11 PST] by Arfaz

--- 66 Plus One/sixsix.c
+++ 66 Plus One/sixsix.c
@@ -1,13 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* plusOne(int* digits, int digitsSize, int* returnSize){
-    int t = 1;
-    int number=0;
-    for (int digitsSize-1; i>-1; i++) {
-        number+=digits[i]*t;
-        t*=10;
+int *plusOne(int *digits, int digitsSize, int *returnSize)
+{
+    for (int i = digitsSize - 1; i >= 0; i--)
+    {
+        if (digits[i] != 9)
+        {
+            ++digits[i];
+            return digits;
+        }
+        digits[i] = 0;
     }
-    ++number;
-    
+    digits[0] = 1;
+    ++digitsSize;
+    for (int i = 1; i > digitsSize; i--)
+    {
+        digits[i] = 0;
+    }
+    *returnSize = digitsSize;
+    return digits;
+}
+
+int main()
+{
+    int *digits = (int *)malloc(3 * sizeof(int));
+    int *returnSize = (int *)malloc(1 * sizeof(int));
+    returnSize[0] = -1;
+    digits[0] = 9;
+    digits[1] = 9;
+    digits[2] = 9;
+    digits[3] = 9;
+    digits[4] = 9;
+    digits[5] = 9;
+    digits[6] = 9;
+    digits[7] = 9;
+    digits[8] = 9;
+    digits[9] = 9;
+    // int digits[] = {1, 2, 3};
+    plusOne(digits, 10, returnSize);
+
+    // plusOne(digits, 3, returnSize);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("\n>>%d ", digits[i]);
+    }
 }
---
+++ 67 Add Binary/sixseven.java
@@ -0,0 +1,34 @@
+class sixseven {
+    private static String addBinary(String a, String b) {
+        StringBuilder c = new StringBuilder();
+        int carry=0;
+        for (int i=0; i<a.length(); i++) {
+            for (int j=0; j<b.length(); j++) {
+                // if ((a.charAt(i) == 1) && (b.charAt(j) == '1') && (carry == 0)) {
+                //     c.setCharAt(i, '0');
+                //     carry = 1;
+                // } else if ((a.charAt(i) == '1') && (b.charAt(j) == '1') && (carry == 1)) {
+                //     c.setCharAt(i, '1');
+                //     carry = 0;
+                // } else if ((carry == 0) && ((a.charAt(i) == 0) && (b.charAt(i) == 1)) || (a.charAt(i) == 1) && (b.charAt(i) == 0)) {
+                //     c.setCharAt(i, '1');
+                // } else if ((carry == 1) && ((a.charAt(i) == 0) && (b.charAt(i) == 1)) || (a.charAt(i) == 1) && (b.charAt(i) == 0)) {
+                //     c.setCharAt(i, '0');
+                //     carry = 1;
+                // } else if ((carry == 0) && (a.charAt(i) == 0) && (b.charAt(i) == 0)) {
+                //     c.setCharAt(i, '0');
+                // } else if ((carry == 1) && (a.charAt(i) == 0) && (b.charAt(i) == 0)) {
+                //     c.setCharAt(i, '1');
+                //     carry = 0;
+                // }
+            }
+            
+        }
+        String cStr = c.toString();
+        System.out.println(">>>["+cStr+"]");
+        return cStr;
+    }
+    public static void main(String[] args) {
+        System.out.println(addBinary("111","111"));
+    }
+}
