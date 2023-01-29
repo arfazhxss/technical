90391f67aba661077766c9515774672549d569db
Arfaz Hussain <arfazhussain@uvic.ca>
2023-01-29 01:19:37 -0800

    Routine Commit Session Committed on: [Sun 29 Jan 2023 01:19:37 PST] by Arfaz

--- 67 Add Binary/sixseven.java
+++ 67 Add Binary/sixseven.java
@@ -4,27 +4,29 @@ class sixseven {
         int carry=0;
         for (int i=0; i<a.length(); i++) {
             for (int j=0; j<b.length(); j++) {
-                // if ((a.charAt(i) == 1) && (b.charAt(j) == '1') && (carry == 0)) {
-                //     c.setCharAt(i, '0');
-                //     carry = 1;
-                // } else if ((a.charAt(i) == '1') && (b.charAt(j) == '1') && (carry == 1)) {
-                //     c.setCharAt(i, '1');
-                //     carry = 0;
-                // } else if ((carry == 0) && ((a.charAt(i) == 0) && (b.charAt(i) == 1)) || (a.charAt(i) == 1) && (b.charAt(i) == 0)) {
-                //     c.setCharAt(i, '1');
-                // } else if ((carry == 1) && ((a.charAt(i) == 0) && (b.charAt(i) == 1)) || (a.charAt(i) == 1) && (b.charAt(i) == 0)) {
-                //     c.setCharAt(i, '0');
-                //     carry = 1;
-                // } else if ((carry == 0) && (a.charAt(i) == 0) && (b.charAt(i) == 0)) {
-                //     c.setCharAt(i, '0');
-                // } else if ((carry == 1) && (a.charAt(i) == 0) && (b.charAt(i) == 0)) {
-                //     c.setCharAt(i, '1');
-                //     carry = 0;
-                // }
+                if ((a.charAt(i) == '1') && (b.charAt(j) == '1') && (carry == 0)) {
+                    // c.setCharAt(i, '0');
+                    carry = 1;
+                } else if ((a.charAt(i) == '1') && (b.charAt(j) == '1') && (carry == 1)) {
+                    // c.setCharAt(i, '1');
+                    carry = 0;
+                } else if ((carry == 0) && ((a.charAt(i) == 0) && (b.charAt(i) == 1)) || (a.charAt(i) == 1) && (b.charAt(i) == 0)) {
+                    // c.setCharAt(i, '1');
+                } else if ((carry == 1) && ((a.charAt(i) == 0) && (b.charAt(i) == 1)) || (a.charAt(i) == 1) && (b.charAt(i) == 0)) {
+                    // c.setCharAt(i, '0');
+                    carry = 1;
+                } else if ((carry == 0) && (a.charAt(i) == 0) && (b.charAt(i) == 0)) {
+                    // c.setCharAt(i, '0');
+                } else if ((carry == 1) && (a.charAt(i) == 0) && (b.charAt(i) == 0)) {
+                    // c.setCharAt(i, '1');
+                    carry = 0;
+                } else {
+                    System.out.println("HERE!");
+                }
             }
-            
         }
         String cStr = c.toString();
+        System.out.println(c);
         System.out.println(">>>["+cStr+"]");
         return cStr;
     }
