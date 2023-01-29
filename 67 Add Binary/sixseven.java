0e0489166c06534001c919488d1ddaba43c17239
Arfaz Hussain <arfazhussain@uvic.ca>
2023-01-29 01:45:28 -0800

    Routine Commit Session Committed on: [Sun 29 Jan 2023 01:45:28 PST] by Arfaz

--- 67 Add Binary/sixseven.java
+++ 67 Add Binary/sixseven.java
@@ -1,36 +1,23 @@
-class sixseven {
-    private static String addBinary(String a, String b) {
-        StringBuilder c = new StringBuilder();
-        int carry=0;
-        for (int i=0; i<a.length(); i++) {
-            for (int j=0; j<b.length(); j++) {
-                if ((a.charAt(i) == '1') && (b.charAt(j) == '1') && (carry == 0)) {
-                    // c.setCharAt(i, '0');
-                    carry = 1;
-                } else if ((a.charAt(i) == '1') && (b.charAt(j) == '1') && (carry == 1)) {
-                    // c.setCharAt(i, '1');
-                    carry = 0;
-                } else if ((carry == 0) && ((a.charAt(i) == 0) && (b.charAt(i) == 1)) || (a.charAt(i) == 1) && (b.charAt(i) == 0)) {
-                    // c.setCharAt(i, '1');
-                } else if ((carry == 1) && ((a.charAt(i) == 0) && (b.charAt(i) == 1)) || (a.charAt(i) == 1) && (b.charAt(i) == 0)) {
-                    // c.setCharAt(i, '0');
-                    carry = 1;
-                } else if ((carry == 0) && (a.charAt(i) == 0) && (b.charAt(i) == 0)) {
-                    // c.setCharAt(i, '0');
-                } else if ((carry == 1) && (a.charAt(i) == 0) && (b.charAt(i) == 0)) {
-                    // c.setCharAt(i, '1');
-                    carry = 0;
-                } else {
-                    System.out.println("HERE!");
-                }
-            }
+public class sixseven {
+    public String addBinary (String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >=0 || j >= 0) {
+            int sum = carry;
+            if (i>=0) sum += a.charAt(i) - '0';
+            if (j >= 0) sum += b.charAt(i) - '0';
+            sb.append(sum%2);
+            carry=sum/2;
+            --i;
+            --j;
         }
-        String cStr = c.toString();
-        System.out.println(c);
-        System.out.println(">>>["+cStr+"]");
-        return cStr;
+        if (carry!=0) sb.append(carry);
+        return sb.reverse().toString();
     }
     public static void main(String[] args) {
-        System.out.println(addBinary("111","111"));
+        // System.out.println('1'-'9');   
     }
 }
