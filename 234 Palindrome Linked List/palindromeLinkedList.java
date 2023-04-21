ef5a30c1fc303ac7c2614ebde6c79b6f78b30ca9
Arfaz Hussain <arfazhussain@uvic.ca>
2023-04-21 03:08:04 -0700

    Routine Commit by @arfazhxss on Fri 21 Apr 2023 03:08:04 PDT

--- 234 Palindrome Linked List/palindromeLinkedList.java
+++ 234 Palindrome Linked List/palindromeLinkedList.java
@@ -49,9 +49,13 @@ public class palindromeLinkedList {
         ListNode storage = null;
         while (root != null) {         // I (storage) null               // II (storage) 5, null           // III (storage) 4, 5, null  -----> (storage) 2, 3, 4, 5, null  
             ListNode temp = root.next; // (root) 5...1 (temp) 4...1      // (root) 4...1 (temp) 3...1      // (root) 3...1 (temp) 2...1 -----> (root) 1 (temp) null
-            root.next = out;           // (root) 5, null                 // (root) 4, 5, null              // (root) 4, 5, null         -----> (root) 1, 2, 3, 4, 5, null  
+            root.next = storage;       // (root) 5, null                 // (root) 4, 5, null              // (root) 4, 5, null         -----> (root) 1, 2, 3, 4, 5, null  
         /*
-         * This makes the main head linkedList cut out, since in the main array [1,2,3,4,5,4,3,2,1], slow is stuck at 5, which is passed as a parameter as 'root', the next of root is slayed to 'null' cutting the main 'head' linkedlist into one [1,2,3,4,5].
+         *
+         * This makes the main head linkedList cut out, since in the main array
+         * [1,2,3,4,5,4,3,2,1], slow is stuck at 5, which is passed as a parameter as
+         * 'root', the next of root is slayed to 'null' cutting the main 'head'
+         * linkedlist into one [1,2,3,4,5].
          */
             storage = root;             // (storage) 5, null            // (storage) 4, 5, null             // (storage) 3, 4, 5, null  -----> (storage) 1, 2, 3, 4, 5, null
             root = temp;                // (root) 4...1                 // (root) 3...1                     // (root) 2...1             -----> (root) null
