8d554b9463714538e1dc3a1793255f0a402571e7
Arfaz Hussain <arfazhussain@uvic.ca>
2023-04-21 01:59:39 -0700

    Routine Commit by @arfazhxss on Fri 21 Apr 2023 01:59:39 PDT

--- 234 Palindrome Linked List/palindromeLinkedList.java
+++ 234 Palindrome Linked List/palindromeLinkedList.java
@@ -1,19 +1,20 @@
 public class palindromeLinkedList {
-    public static boolean isPalindrome (ListNode head) {
-        if (head==null) return true;
+    public static boolean isPalindrome(ListNode head) {
+        if (head == null)
+            return true;
         ListNode fast = head;
         ListNode slow = head;
 
         System.out.print("head1:\t\t");
         printA(head);
 
-        while (fast.next!=null) {
+        while (fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
-        
+
         // TestCode//ListNode temp = null;
-        ListNode temp = reverseLinkedList (slow);
+        ListNode temp = reverseLinkedList(slow);
 
         System.out.print("head2:\t\t");
         printA(head);
@@ -21,8 +22,9 @@ public class palindromeLinkedList {
         System.out.print("temp:\t\t");
         printA(temp);
 
-        while (temp!=null) {
-            if (temp.val!=head.val) return false;
+        while (temp != null) {
+            if (temp.val != head.val)
+                return false;
             else {
                 temp = temp.next;
                 head = head.next;
@@ -30,32 +32,40 @@ public class palindromeLinkedList {
         }
         return true;
     }
-    public static ListNode reverseLinkedList (ListNode root) {
-        ListNode out=null;
-        while (root!=null) {
+
+    public static ListNode reverseLinkedList(ListNode root) {
+        ListNode out = null;
+        while (root != null) {
             ListNode temp = root.next;
-            root.next = out; // -----------------------------------> This makes the main head linkedList cut out, since in out arr [1,2,3,4,5,4,3,2,1], 
-            out = root;      //                                         slow is stuck at 5, which is passed as a parameter as 'root', the next of root is slayed to 'null'
-            root = temp;     //                                         cutting the main 'head' linkedlist into one [1,2,3,4,5].
+            root.next = out; 
+            /* This makes the main head linkedList cut out, since in the main array [1,2,3,4,5,4,3,2,1], 
+            slow is stuck at 5, which is passed as a parameter as 'root', the next of root is slayed to 'null' 
+            cutting the main 'head' linkedlist into one [1,2,3,4,5]. */
+            out = root;
+            root = temp; 
         }
         return out;
     }
+
     // TEST
-    public static void addA (ListNode root, int value) {
-        while (root.next!=null) {root=root.next;}
-        ListNode curr = new ListNode(value,null);
-        root.next=curr;
+    public static void addA(ListNode root, int value) {
+        while (root.next != null) {
+            root = root.next;
+        }
+        ListNode curr = new ListNode(value, null);
+        root.next = curr;
     }
 
-    public static void printA (ListNode root) {
+    public static void printA(ListNode root) {
         ListNode curr = root;
         do {
-            System.out.print(curr.val+" ");
-            curr=curr.next;
-        } while (curr!=null);
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        } while (curr != null);
 
         System.out.println("[]");
     }
+
     public static void main(String[] args) {
         ListNode mainlist = new ListNode(1, null);
         addA(mainlist, 2);
@@ -67,6 +77,6 @@ public class palindromeLinkedList {
         addA(mainlist, 2);
         addA(mainlist, 1);
         // printA(mainlist);
-        System.out.println("ActualValue:\t"+isPalindrome(mainlist));
+        System.out.println("ActualValue:\t" + isPalindrome(mainlist));
     }
 }
