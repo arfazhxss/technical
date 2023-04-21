459f3f7b1c66f5e00efe007128b8d6cc0e61bfdb
Arfaz Hussain <arfazhussain@uvic.ca>
2023-04-21 16:55:36 -0700

    Routine Commit by @arfazhxss on Fri 21 Apr 2023 16:55:36 PDT

---
+++ 118 Pascal's Triangle/E118.java
@@ -0,0 +1,11 @@
+public class E118 {
+    public static List<List<Integer>> generate (int numRows)
+    {
+        return null;
+    }
+    public static void main(String[] args) {
+        System.out.println(" - - E118 Test - - \n");
+
+        System.out.println("\n - - - - - - - - - \n\n");
+    }
+}
