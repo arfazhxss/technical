6969a0761edfe92984b75fbb8858d40a98d67071
Arfaz Hussain <arfazhussain@uvic.ca>
2023-04-21 16:14:05 -0700

    Routine Commit by @arfazhxss on Fri 21 Apr 2023 16:14:05 PDT

--- 125 Valid Palindrome/E125.java
+++ 125 Valid Palindrome/E125.java
@@ -1,7 +1,29 @@
 public class E125 {
-    public static void main(String[] args) {
-        System.out.println(" - - E125 Test - - ");
+    public static boolean isPalindromeOne (String s) 
+    {
+        String newString = "";
+        for (Char c : s.toCharArray())
+        {
+            if (Character.isLetter(c)||Character.isDigit(c)) {newString+=c;}
+        }
+        /*
+         * Now we hae newString without any extra spaces and anything other than letters and digits
+         */
+        int start = 0;
+        int end = newString.length()-1;
+        while (start>end) 
+        {
+            if (newString.charAt(start)!=newString.charAt(end)) {return false;}
+            start++;
+            end--;
+        }
+        return true;
 
+    }
+    public static void main(String[] args) {
+        System.out.println(" - - E125 Test1 - - ");
+        String testStringOne = "A man, a plan, a canal: Panama";
+        System.out.println("testing String \""+testStringOne+"\":"+isPalindromeOne(testStringOne));
         System.out.println(" - - - - - - - - - ");
     }
 }
