7274c76965ca52049b095cf6f45240197f0de88d
Arfaz Hussain <arfazhussain@uvic.ca>
2023-04-21 16:19:24 -0700

    Routine Commit by @arfazhxss on Fri 21 Apr 2023 16:19:24 PDT

--- 125 Valid Palindrome/E125.java
+++ 125 Valid Palindrome/E125.java
@@ -2,16 +2,20 @@ public class E125 {
     public static boolean isPalindromeOne (String s) 
     {
         String newString = "";
-        for (Char c : s.toCharArray())
+        for (char c : s.toCharArray())
         {
             if (Character.isLetter(c)||Character.isDigit(c)) {newString+=c;}
         }
         /*
          * Now we hae newString without any extra spaces and anything other than letters and digits
          */
+        newString=newString.toLowerCase();
+        /*
+         * And now all of them are smaller letters so that we can write tit without discriminatory capital letters
+         */
         int start = 0;
         int end = newString.length()-1;
-        while (start>end) 
+        while (start<end) 
         {
             if (newString.charAt(start)!=newString.charAt(end)) {return false;}
             start++;
@@ -21,9 +25,11 @@ public class E125 {
 
     }
     public static void main(String[] args) {
-        System.out.println(" - - E125 Test1 - - ");
+        System.out.println(" - - E125 Test1 - - \n");
         String testStringOne = "A man, a plan, a canal: Panama";
-        System.out.println("testing String \""+testStringOne+"\":"+isPalindromeOne(testStringOne));
-        System.out.println(" - - - - - - - - - ");
+        String testStringTwo = "race a car";
+        System.out.println("["+testStringOne+"]: "+isPalindromeOne(testStringOne));
+        System.out.println("[" + testStringTwo + "]: " + isPalindromeOne(testStringTwo));
+        System.out.println("\n - - - - - - - - - ");
     }
 }
