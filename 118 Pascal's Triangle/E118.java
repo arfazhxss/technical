fe0b7ac844dc1e16b70218f3b9cd931c56f2d8f3
Arfaz Hussain <arfazhussain@uvic.ca>
2023-04-21 17:00:48 -0700

    Routine Commit by @arfazhxss on Fri 21 Apr 2023 17:00:48 PDT

--- 118 Pascal's Triangle/E118.java
+++ 118 Pascal's Triangle/E118.java
@@ -3,19 +3,19 @@ import java.util.ArrayList;
 public class E118 {
     public static List<List<Integer>> generate (int numRows)
     {
-        List<List<Integer>> output = new ArrayList<>();
-        List<Integer> cur = new ArrayList<>();
+        List<List<Integer>> outerList = new ArrayList<>();
+        List<Integer> innerList = new ArrayList<>();
 
         for (int i=0; i<numRows; i++) 
         {
-            cur.add(0,1); // index, object
-            for (int j=1; j<cur.size()-1; j++)
+            innerList.add(0,1); // index, object
+            for (int j=1; j<innerList.size()-1; j++)
             {
-                cur.set(j,(cur.get(j)+cur.get(j+1)));
+                innerList.set(j,(innerList.get(j)+innerList.get(j+1)));
             }
-            output.add(new ArrayList<>(cur));
+            outerList.add(new ArrayList<>(innerList));
         }
-        return output;
+        return outerList;
     }
     public static void main(String[] args) {
         System.out.println(" - - E118 Test - - \n");
