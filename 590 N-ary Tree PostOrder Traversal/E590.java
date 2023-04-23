9758d192a7a8c22e5280ab72bc55654945e80673
Arfaz Hussain <arfazhussain@uvic.ca>
2023-04-22 23:11:17 -0700

    Routine Commit by @arfazhxss on Sat 22 Apr 2023 23:11:17 PDT

--- 590 N-ary Tree PostOrder Traversal/E590.java
+++ 590 N-ary Tree PostOrder Traversal/E590.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+
 public class E590 {
-    
+    public static List<Integer> postorder (Node root) {
+        List<Integer> output = new ArrayList<>();
+        helper(root,output);
+        return output;
+    }
+
+    public void helper (Node root, List<Integer> output) {
+        if (root==null) return;
+        for (int i=0; i < root.children.size(); i++) {
+            helper (root.childre.get(i),output);
+        }
+        output.add(root.val);
+    }
     public static void main(String[] args) {
         
     }
