35262c187778d0037ae183553e345914e27d805a
Arfaz Hussain <arfazhussain@uvic.ca>
2023-04-29 22:58:13 -0700

    Routine Commit by @arfazhxss on Sat 29 Apr 2023 22:58:13 PDT

---
+++ 0074 Search A 2D Matrics/E74.java
@@ -0,0 +1,7 @@
+public class E74 {
+    
+    public static void main(String[] args) {
+        System.out.println("E1047: No Test Implemented");
+        System.out.println("Task NOT Accomplished");
+    }
+}
