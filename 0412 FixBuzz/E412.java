d73a33b1a3f7409c205f234329e4811bd7cdb9d2
Arfaz Hussain <arfazhussain@uvic.ca>
2023-04-29 23:31:05 -0700

    Routine Commit by @arfazhxss on Sat 29 Apr 2023 23:31:05 PDT

---
+++ 0412 FixBuzz/E412.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class E412 {
+    public static List<String> fizzBuzz(int n) {
+        List<String> output = new ArrayList<String>();
+        for (int i=1, fizz=0, buzz=0; i<=n; i++) {
+            fizz++;
+            buzz++;
+            if (fizz==3 && buzz==5) {
+                fizz=0;
+                buzz=0;
+                output.add("FizzBuzz");
+            } else if (fizz==3) {
+                fizz=0;
+                output.add("Fizz");
+            } else if (buzz==5) {
+                buzz=0;
+                output.add("Buzz");
+            } else {
+                output.add(Integer.toString(i));
+            }
+        }
+        return output;
+    }
+    public static void main(String[] args) {
+        System.out.println("E412: Test Implemented Below:");
+        fizzBuzz(5);
+        System.out.println("Task NOT Accomplished");
+    }
+}
