4d7f8eac7a6d27cfe4e4fd7d27a72700cccb8b68
Arfaz Hussain <arfazhussain@uvic.ca>
2023-04-29 23:01:36 -0700

    Routine Commit by @arfazhxss on Sat 29 Apr 2023 23:01:36 PDT

--- 0074 Search A 2D Matrics/E74.java
+++
@@ -1,7 +0,0 @@
-public class E74 {
-    
-    public static void main(String[] args) {
-        System.out.println("E1047: No Test Implemented");
-        System.out.println("Task NOT Accomplished");
-    }
-}
---
+++ 0074 Search A 2D Matrix/E74.java
@@ -0,0 +1,15 @@
+public class E74 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrics.length==0) return false;
+
+        int row = matrix.length;
+        int columns = matrix[0].length;
+        int left=0;
+        int right = rows.columns-1;
+
+    }
+    public static void main(String[] args) {
+        System.out.println("E1047: No Test Implemented");
+        System.out.println("Task NOT Accomplished");
+    }
+}
