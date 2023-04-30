c00fbb43121e0c6c9ef89d0252aa094dc06083b7
Arfaz Hussain <arfazhussain@uvic.ca>
2023-04-30 00:18:51 -0700

    Routine Commit by @arfazhxss on Sun 30 Apr 2023 00:18:51 PDT

--- 0482 License Key Formatting/E482.java
+++ 0482 License Key Formatting/E482.java
@@ -1,10 +1,14 @@
-public class E485 {
+public class E482 {
     public String licenseKeyFormatting (String S, int K) {
         S = S.toUpperCase();
         S = S.replaceAll("-","");
         
         StringBuilder Str = new StringBuilder(S);
-        
+
+        for (int i = S.length()-K; i>0; i-=K) {
+            Str.insert(i,"-");
+        }
+        return Str.toString();
     }
     public static void main(String[] args) {
         System.out.println("No Test Implemented");
