d868664e776f35b00ada5423a43d2fc0a824d0ad
Arfaz Hussain <arfazhussain@uvic.ca>
2023-04-29 22:42:21 -0700

    Routine Commit by @arfazhxss on Sat 29 Apr 2023 22:42:21 PDT

--- 1047 Remove All Adjacent Duplicates/E1047.java
+++ 1047 Remove All Adjacent Duplicates/E1047.java
@@ -1,3 +1,17 @@
 public class E1047 {
+    public String removeDuplicates (String S) {
+        char[] stack = new chr[S.length()];
+        int i = 0;
 
+        for (int j=0; j<S.length(); j++) {
+            char current_char = S.charAt(j);
+
+            if ((i>0) && stack[i-1]==current_char) { i--; }
+            else {
+                stack[i] = current_char;
+                i++;
+            }
+        }
+        return new String(stack, 0, i);
+    }
 }
