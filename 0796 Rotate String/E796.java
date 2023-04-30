20028e000a7fb28fd7ba21b74ff21a782f6276b8
Arfaz Hussain <arfazhussain@uvic.ca>
2023-04-29 23:52:20 -0700

    Routine Commit by @arfazhxss on Sat 29 Apr 2023 23:52:20 PDT

---
+++ 0796 Rotate String/E796.java
@@ -0,0 +1,9 @@
+public class E796 {
+    public boolean rotateString (String A, String B) {
+        return ((A.length()==B.length()) && (A+A).contains(B));
+    }
+    public static void main(String[] args) {
+        System.out.println("E796: Test NOT Implemented");
+        System.out.println("Task NOT Performed");
+    }
+}
