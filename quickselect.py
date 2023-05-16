dd06160365333f1e4f1aef4723d2abe2e6e07ece
Arfaz Hossain <arfazhussain@uvic.ca>
2023-05-15 21:12:53 -0700

    Routine Commit by @arfazhxss on Mon 15 May 2023 21:12:53 PDT

--- quickselect.py
+++ quickselect.py
@@ -1,8 +1,14 @@
 class solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        k = len(num) - k
+        k = len(nums) - k
 
 
 def quickSelect(l, r):
     pivot, p = nums[r], lfor i in range(l, r):
     if (nums[i]) <= pivot:
+        nums[p], ums[i] = nums[i], nums[p]
+        p += 1
+    nums[p], nums[r] = nums[r], nums[p]
+    
+    if p > k:
+
