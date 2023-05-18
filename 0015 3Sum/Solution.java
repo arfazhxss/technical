dde2c5e8f13e1405f72c6e54a491ec22e746abbd
Arfaz Hossain <arfazhussain@uvic.ca>
2023-05-18 15:04:39 -0700

    Routine Commit by @arfazhxss on Thu 18 May 2023 15:04:39 PDT

--- 0015 3Sum/Solution.java
+++ 0015 3Sum/Solution.java
@@ -3,18 +3,18 @@ import java.util.*;
 public class Solution {
     public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> finalList = new LinkedList<List<Integer>>();
-        Arrays.sort(nums);
+        Arrays.sort(nums);    // Sorting the array because we need to because 1>> SKIP if we find a similar pivot 2>> Left and Right Pointer functionality
 
         for (int i=0; i<nums.length-2; i++) {
-            if ((i>0)&&(nums[i]==nums[i-1])) {continue;}
-            int leftPointer = i+1;
-            int rightPointer = nums.length-1;
-            int neededTotal = 0 - nums[i];
-            while (leftPointer<rightPointer) {
-                int currTotal = nums[leftPointer] + nums[rightPointer];
-                if (currTotal<neededTotal) {++leftPointer;}
-                else if (currTotal>neededTotal) {--rightPointer;}
-                else {
+            if ((i>0)&&(nums[i]==nums[i-1])) {continue;}    // SKIPPING if the two pivots back to back are the same
+            int leftPointer = i+1; // i = pivot, left pointer starting from the next of pivot
+            int rightPointer = nums.length-1;   // right pointer starting from the rightmost side of the array
+            int neededTotal = 0 - nums[i]; // we want the total sum to be 0, so we want the final to be 0, negative the pivot
+            while (leftPointer<rightPointer) { // looping until they aren't the same numbers
+                int currTotal = nums[leftPointer] + nums[rightPointer]; // making a number that adds upto the total of left and right, that number plus pivot should be 0
+                if (currTotal<neededTotal) {++leftPointer;} // checking if that number comes down to the needed number without the pivot: if more, increase the left pointer!
+                else if (currTotal>neededTotal) {--rightPointer;}   // checking if that number comes down to the needed number without the pivot: if less, decrease the right pointer!
+                else { // this is in case they both add upto the expected sum
                     finalList.add(List.of(nums[i],nums[leftPointer],nums[rightPointer]));
                     ++leftPointer;
                     while ((leftPointer<rightPointer)&&(nums[leftPointer]==nums[leftPointer-1])) {++leftPointer;}}
