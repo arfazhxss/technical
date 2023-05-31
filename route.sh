ffd783ee84575a30ee37b6de6c093102fa6ae3a1
Arfaz Hossain <arfazhussain@uvic.ca>
2023-05-30 17:33:10 -0700

    Route Mod Commit by @arfazhxss on Tue 30 May 2023

--- route.sh
+++ route.sh
@@ -2,20 +2,20 @@
 #auto-git v4.5
 
 echo "Commit Message:"
-read commit_message
+read cmss
 
-if [[ -z "$commit_message" || ${#commit_message} -lt 3 ]]; then
-  commit_message="Routine Commit"
+if [[ -z "$cmss" || ${#cmss} -lt 3 ]]; then
+  cmss="Routine Commit"
   git add . && \
   git add -u && \
-  git commit -m $"$commit_message"$'\nby @arfazhxss on '"$(date +'%a %d %b %Y')" && \
+  git commit -m $"$cmss"$'\nby @arfazhxss on '"$(date +'%a %d %b %Y')" && \
   git push origin HEAD
   git log > commit-hist.txt
   rm -rf .DS_Store/
 else
   git add . && \
   git add -u && \
-  git commit -m $"$commit_message"$'\nCommit by @arfazhxss on '"$(date +'%a %d %b %Y')" && \
+  git commit -m $"$cmss"$'\nCommit by @arfazhxss on '"$(date +'%a %d %b %Y')" && \
   git push origin HEAD
   git log > commit-hist.txt
   rm -rf .DS_Store/
