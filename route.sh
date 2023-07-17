42b26659a136126531232068a3f55a2f4208e889
arfazhxss <arfazhussain@uvic.ca>
2023-07-17 09:36:17 -0700

    Workflow Version Update Commit by @arfazhxss on Mon 17 Jul 2023

--- route.sh
+++ route.sh
@@ -1,38 +1,58 @@
 #!/bin/bash
-#auto-git v6.2
+# git workflow v6.8
+# 2022-2023 @arfazhxss
+
+unEnScore=50
+unEnString=$(printf '_%.0s' $(seq 1 "$unEnScore"))
+
+function versionCheck() {
+  dir="$(dirname "$0")/.git"
+  if [ ! -d "$dir" ]
+  then
+    echo -e "No Version Control History Found\nInitializing Git Version Control"
+    git --version 
+    git init
+    flag=1
+  else
+    echo -e "\nVersion Control History found"
+  fi
+}
+
+
+function syncBranch() {
+  echo -e "YES'ED\n${unEnString}"
+  git stash
+  git stash clear
+  git pull
+  echo -e "${unEnString}\n\t\tYour Repository is synced\n\t\twith the latest commit :)\n${unEnString}"
+}
+
+function pushChanges() {
+  git add . && \
+  git add -u && \
+  git commit -m $"$commt"$'\nCommit by @arfazhxss on '"$(date +'%a %d %b %Y')" && \
+  git push origin HEAD
+  git log > commit-hist.txt
+  rm -Rf .DS_Store/
+  echo -e "${unEnString}\n\t\tYour changes have been pushed\n\t\tto the repository :)\n${unEnString}"
+}
 
 find . -name ".DS_Store" -type f -delete
 echo -e ".DS_Store\ncommit-hist.txt" > .gitignore
-
-echo -e "\n_______________________________________________________\n\n\t\tDELETE LOCAL CHANGES? (YES) \n\t\t\tOR\n\t\tPUSH LOCAL CHANGES (ENTER)\n"
+versionCheck
+echo -e "\n${unEnString}\n\n\t\tDELETE LOCAL CHANGES? (YES) \n\t\t\tOR\n\t\tPUSH LOCAL CHANGES (ENTER)\n"
 read -s -n 3 -p "(yes/ENTER): " answer
 
 if [[ $answer == "yes" || $answer == "Yes" || $answer == "YES" ]]; then
-  echo -e "YES'ED\n-------------------------------------------------------"
-  git stash
-  git stash clear
-  git pull origin main
-  echo -e "-------------------------------------------------------\n\t\tYour Repository is synced\n\t\twith the latest commit :)\n_______________________________________________________"
+  syncBranch
 else
-  echo -e "ENTER'ED\n-------------------------------------------------------"
+  echo -e "ENTER'ED\n${unEnString}"
   read -p "Your Commit Message: " commt
 
   if [[ -z "$commt" || ${#commt} -lt 3 ]]; then
     commt="Routine Commit"
-    git add . && \
-    git add -u && \
-    git commit -m $"$commt"$'\nby @arfazhxss on '"$(date +'%a %d %b %Y')" && \
-    git push origin HEAD
-    git log > commit-hist.txt
-    rm -Rf .DS_Store/
-    echo -e "-------------------------------------------------------\n\t\tYour changes has been pushed\n\t\tto the repository :)\n_______________________________________________________"
+    pushChanges
   else
-    git add . && \
-    git add -u && \
-    git commit -m $"$commt"$'\nCommit by @arfazhxss on '"$(date +'%a %d %b %Y')" && \
-    git push origin HEAD
-    git log > commit-hist.txt
-    rm -Rf .DS_Store/
-    echo -e "-------------------------------------------------------\n\t\tYour changes has been pushed\n\t\tto the repository :)\n_______________________________________________________"
+    pushChanges
   fi
 fi
