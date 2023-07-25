57da306aefb2d15f0dda47706c5164b5ba2244c5
arfazhxss <arfazhussain@uvic.ca>
2023-07-24 20:04:43 -0700

    Routine Commit Commit by @arfazhxss on Mon 24 Jul 2023

--- 0242 Valid Anagram/Solution1.py
+++ 0242 Valid Anagram/Solution1.py
@@ -6,8 +6,8 @@ class Solution:
         countS, countT = {}, {}
 
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
+            countS[s[i]] = 1 + countS.get(s[i], 0) # countS -> occurance of each character in string S
+            countT[t[i]] = 1 + countT.get(t[i], 0) # countT -> occurance of each character in string T
         for c in countS:
             if countS[c] != countT.get(c, 0):
                 return False
