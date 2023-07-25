7230899302ed6069a8b4f0658b0cfc9323eb6565
arfazhxss <arfazhussain@uvic.ca>
2023-07-24 20:14:30 -0700

    Routine Commit Commit by @arfazhxss on Mon 24 Jul 2023

--- 0242 Valid Anagram/Solution1.py
+++ 0242 Valid Anagram/Solution1.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
@@ -8,8 +8,61 @@ class Solution:
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i], 0) # countS -> occurance of each character in string S
             countT[t[i]] = 1 + countT.get(t[i], 0) # countT -> occurance of each character in string T
-        for c in countS:
-            if countS[c] != countT.get(c, 0):
+        for c in countS: # iterating through the keys
+            if countS[c] != countT.get(c, 0): 
                 return False
-            
+        
         return True
+
+def test_case_1():
+    sol = Solution1()
+    s = "listen"
+    t = "silent"
+    assert sol.isAnagram(s, t) == True
+
+def test_case_2():
+    sol = Solution1()
+    s = "hello"
+    t = "world"
+    assert sol.isAnagram(s, t) == False
+
+def test_case_3():
+    sol = Solution1()
+    s = "anagram"
+    t = "nagaram"
+    assert sol.isAnagram(s, t) == True
+
+def test_case_4():
+    sol = Solution1()
+    s = "hello"
+    t = "ohhello"
+    assert sol.isAnagram(s, t) == False
+
+def test_case_5():
+    sol = Solution1()
+    s = ""
+    t = ""
+    assert sol.isAnagram(s, t) == True
+
+def test_case_6():
+    sol = Solution1()
+    s = "iceman!"
+    t = "!cinema"
+    assert sol.isAnagram(s, t) == True
+
+def test_case_7():
+    sol = Solution1()
+    s = "hello"
+    t = "world!"
+    assert sol.isAnagram(s, t) == False
+
+if __name__ == "__main__":
+    test_case_1()
+    test_case_2()
+    test_case_3()
+    test_case_4()
+    test_case_5()
+    test_case_6()
+    test_case_7()
+
+    print("All test cases passed!")
