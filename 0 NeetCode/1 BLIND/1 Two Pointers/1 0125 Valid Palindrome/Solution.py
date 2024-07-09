b641762d0cc8df6e7bd8eab0ede6fd02e1597f77
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-07-08 23:54:33 -0700

    Routine Commit Commit by @arfazhxss on Mon 08 Jul 2024

--- 0 NeetCode/1 BLIND/1 Two Pointers/1 0125 Valid Palindrome/Solution.py
+++ 0 NeetCode/1 BLIND/1 Two Pointers/1 0125 Valid Palindrome/Solution.py
@@ -2,22 +2,18 @@ class Solution:
     # Cheating Solution
     # def isPalindrome (self, s: str) -> bool:
     #     newStr = ""
-
     #     for c in s:
     #         if c.isalnum():
     #             newStr += c.lower()
-        
     #     return newStr == newStr[::-1]
     
     # Memory Efficient Solution
     def isPalindrome (self, s: str) -> bool:
         l, r = 0, len(s) - 1
 
-        # case when there's a non-alphaneumeric
         while l < r and not self.alphaNum(s[l]):
             l += 1
 
-        # case when there's a non-alphaneumeric
         while r > 1 and not self.alphaNum(s[r]):
             r -= 1
 
@@ -31,3 +27,11 @@ class Solution:
 
     def alphaNum(self, c):
         return ((ord('A') <= ord(c) <= ord('Z')) or (ord('a') <= ord(c) <= ord('z')) or (ord(0) <= ord(c) <= ord(9)))
+    
+    def isPalindrome2 (self, s: str) -> bool:
+        ss=[char.lower() for char in s[0:len(s)//2] if char.isalnum()]
+        tt = [char.lower() for char in s[len(s)//2:][::-1] if char.isalnum()]
+        for i, char in enumerate(tt):
+            if (i<len(ss)) and (char != ss[i]):
+                return False
+        return True
